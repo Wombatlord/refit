2d354b2776251679973cc0bf131604c88f5b4bb0
Wombatlord <jordan.stone0@gmail.com>
2024-02-11 19:14:03 +0000

    Working profile switching with error messaging

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use std::{
-    env::args,
+    env::{args, current_exe},
     fs::{read_dir, rename, File},
     io::{self, BufRead, BufReader, Write},
 };
 
 fn main() {
     let chosen_palette = args().nth(1).expect("No palette selected.");
-
-    let configs_path = match File::open("./refit.conf") {
+    let current_loc = current_exe().unwrap();
+    let cl = current_loc.parent().unwrap();
+    let configs_path = match File::open(format!("{}/refit.conf", cl.to_str().unwrap())) {
         Ok(f) => BufReader::new(f)
             .lines()
             .next()
             .expect("Error reading file")
             .expect("Empty file"),
         Err(_) => {
-            let mut refit = File::create("refit.conf").expect("Failed to create refit.conf");
-            println!("Created refit.conf file. Please enter path to your starship.toml directory.");
+            println!("No config found. Please enter path to your starship.toml directory.");
 
             let mut input = String::new();
             io::stdin()
                 .read_line(&mut input)
                 .expect("error: unable to read user input");
+            let mut refit =
+                File::create(format!("{}/refit.conf", input.clone().trim().to_string()))
+                    .expect("Failed to create refit.conf");
             refit
                 .write(input.as_bytes())
                 .expect("Failed to write to refit.conf.");
             input.to_string().trim().to_string()
         }
     };
-    let e = &format!("starship.toml not found.\n{} AAAA", configs_path);
+    let e = &format!("starship.toml not found at {}", configs_path);
     let starship = File::open(format!("{}/starship.toml", configs_path)).expect(e);
     let first_line = BufReader::new(starship)
         .lines()
@@ -36,32 +39,41 @@ fn main() {
         .expect("failed to read line");
     let palette_name = first_line.split(" ").collect::<Vec<&str>>();
 
-    match rename(
-        format!("{}/starship.toml", configs_path),
-        format!("{}/starship.{}.toml", configs_path, palette_name[1]),
-    ) {
-        Ok(_) => {}
-        Err(e) => eprint!("{e}"),
-    };
-
-    let configs_dir = read_dir(configs_path.clone()).unwrap();
-
-    for file in configs_dir {
-        match file {
-            Ok(f) => {
-                if f.file_name().to_str().unwrap().trim()
-                    == format!("starship.{}.toml", chosen_palette)
-                {
-                    rename(
-                        format!("{}/starship.{}.toml", configs_path, chosen_palette),
-                        format!("{}/starship.toml", configs_path),
-                    )
-                    .expect("Failed to rename starship.%.toml");
-                } else {
-                    continue;
-                }
+    let mut configs_dir = read_dir(configs_path.clone()).unwrap();
+    let profile = configs_dir.find_map(|f| match f {
+        Ok(fl) => {
+            if fl.file_name().to_str().unwrap().trim()
+                == format!("starship.{}.toml", chosen_palette)
+            {
+                Some(fl)
+            } else {
+                None
             }
-            Err(e) => eprintln!("{e}"),
         }
+        Err(e) => {
+            eprintln!("{e}");
+            std::process::exit(1)
+        }
+    });
+
+
+    if profile.is_some() {
+        {
+            match rename(
+                format!("{}/starship.toml", configs_path),
+                format!("{}/starship.{}.toml", configs_path, palette_name[1]),
+            ) {
+                Ok(_) => {}
+                Err(e) => eprint!("{e}"),
+            };
+
+            rename(
+                format!("{}/starship.{}.toml", configs_path, chosen_palette),
+                format!("{}/starship.toml", configs_path),
+            )
+            .expect("Failed to rename starship.%.toml");
+        } 
+    } else {
+        eprintln!("No profile matching {} found in {}", chosen_palette, configs_path)
     }
 }
